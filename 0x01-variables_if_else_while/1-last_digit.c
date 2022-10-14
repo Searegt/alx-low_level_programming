954edc621111add7e24a58dd010fda84ec7ded63
Searegt <seareg10@gmail.com>
2022-10-14 06:00:01 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+
+	printf("What if you age");
+	scan("%d", &age);
+
+	if (age > 18)
+	{
+		printf("You are good to and bet");
+	} else if (age >= 0 && age < 18)
+	{
+		printf("Sorry You cannot bet");
+	} else if (age < 0)
+	{
+		printf("Sorry you are not born yet");
+	} else
+	{
+		printf("Sorry, we know nothing about you");
+	}
+	return (0);
+}
+
