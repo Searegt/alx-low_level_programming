6f9fedfa85d08b94d8c7eae41e76434ae1e60222
Searegt <seareg10@gmail.com>
2022-10-14 06:33:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char grade;
+
+	printf("Please enter your ypu grade to see remark");
+	scanf("%c, &grade);
+
+	switch(grade)
+	case 'A':
+		printf("You are excellent student");
+		break;
+	case 'B':
+		printf("Good studentt");
+		break;
+	case 'C':
+		printf("You did well");
+		break;
+	default:
+		printf("No remark");
+		return (0);
+}
