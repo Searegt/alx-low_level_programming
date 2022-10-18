85f7187b767db4fc47fed37cdd31a18c4db2b80a
Searegt <seareg10@gmail.com>
2022-10-18 06:02:30 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+_putchar file('_');
+_putchar file('p');
+_putchar file('u');
+_putchar file('t');
+_putchar file('c');
+_putchar file('h');
+_putchar file('r');
+_putchar file('\n');
+return (0);
+}
