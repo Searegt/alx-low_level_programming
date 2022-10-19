96ce068f07a0f059fc020481621ad2b261035150
Searegt <seareg10@gmail.com>
2022-10-19 04:58:39 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 20; h++)
+	for (h = 0; h <= 24; h++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (m = 0; m <= 60; m++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
