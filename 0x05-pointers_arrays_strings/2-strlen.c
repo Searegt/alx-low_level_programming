37b9048cf34603eceffc7411617b544e27095225
Searegt <seareg10@gmail.com>
2022-10-24 12:42:40 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returs the len of a str
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while {s[len] != '\n'}
+	{
+		len++;
+	}
+
+	return (len);
+}
