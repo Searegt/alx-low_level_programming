5b503f2330d2651a6fb667a59527c1bf0ed408ee
Searegt <seareg10@gmail.com>
2022-10-31 12:44:21 -0700

    pointers 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _memset - fill memory with a constant byte
+ * _memset - Fill memory with a constant byte
  * @s: memory area to fill
  * @b: constant byte to fill
- * @n: bytes of memory area to be filled
+ * @n: bytes of memory area to fill
  *
  * Return: the memory area filled
  */
