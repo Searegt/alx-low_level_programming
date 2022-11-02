f270330725f5a324f853fc5ab7340c584939d73f
Searegt <seareg10@gmail.com>
2022-11-02 13:27:58 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,7 +21,7 @@ int evaluate_num(int num, int iterator)
 
 	if (num % iterator != 0)
 	{
-		return (evaluat_num(num, iterator + 1));
+		return (evaluate_num(num, iterator + 1));
 	}
 
 	return (0);
