69b2aebc36ada277e7cfa18981a881ddb59aa8d5
Searegt <seareg10@gmail.com>
2022-11-04 13:26:50 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,3 +27,44 @@ int check_num(char *str)
 	}
 	return (1);
 }
+
+/**
+ * main - print the name of te program
+ * @argc: count argument
+ * @argv: arguments
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count])
+
+				{
+				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+				sum += str_to_int;
+				}
+
+				/*condition if one of the number contains symbols that are not digits*/
+				else
+				{
+				printf("Error\n");
+				return (1);
+				}
+
+				count++;
+				}
+
+				printf("%d\n". sum); /*print sum*/
+
+				return (0);
+				}
