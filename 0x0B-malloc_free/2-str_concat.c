8124823bd0bbd01f0c53360689821f294e9ab1a0
Searegt <seareg10@gmail.com>
2022-11-07 11:10:12 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 = NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	/*count size total*/
@@ -52,7 +52,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; *(s1 + i) != '\0', i++)
 		*(dst + i) = *(s1 + i);
 
-	for (j = 0; *(s2 + i) != '\0', j++)
+	for (j = 0; *(s2 + j) != '\0', j++)
 	{
 		*(dst + i) = *(s2 + j);
 		i++;
